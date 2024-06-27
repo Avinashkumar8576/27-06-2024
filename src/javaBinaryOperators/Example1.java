df8590c151d4e7911494aef9c0bc67f87ae0252c
Avinashkumar8576 <avinashkumar90089008@gmail.com>
2024-06-27 13:55:27 +0530

    first commit

---
+++ src/javaBinaryOperators/Example1.java
@@ -0,0 +1,15 @@
+package javaBinaryOperators;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// Logical/Conditional Operators
+		
+		boolean value = false;
+		
+		System.out.println(!value);
+
+	}
+
+}
+
---
+++ src/javaBinaryOperators/Example3.java
@@ -0,0 +1,24 @@
+package javaBinaryOperators;
+
+public class Example3
+{
+	
+	public static void main(String[] args) {
+		
+		
+		/**
+		 * Write a java program to verify whether the given is divisible by 4 and 5
+		 * 
+		 * Note: the number is 450.
+		 * 
+		 */
+		
+		int number = 450;
+		
+		boolean condition = (number%10 == 0 && number%5 == 0);
+		
+		System.out.println(condition);
+		
+	}
+
+}
